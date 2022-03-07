84fdc52f43eda8dd2b5ecfe5ab6707e5b5a7e61e
AntonWarming <95622901+AntonWarming@users.noreply.github.com>
2022-03-07 14:52:02 +0100

    Slideshow

--- main.js
+++ main.js
@@ -9,3 +9,23 @@
    //var col=document.getElementById("breadcrumbing1");
    //col.style.backgroundColor="#00000000";
    //}
+
+   var i = 0;
+   var images = [];
+
+
+   images[0] = 'billeder/facebook.png';
+   images[1] = 'billeder/ttnl.jpg';
+   images[2] = 'billeder/Twitter.png';
+
+   function skift(){
+     document.slide.src = images[i];
+
+     if(i < images.length - 1){
+       i++;
+     } else {
+       i = 0;
+     }
+   }
+
+   window.onload = skift;
