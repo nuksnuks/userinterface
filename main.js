c5643d004f671b64e789240e75176d794d2aa7b7
nuksnuks <davidwogelius@gmail.com>
2022-03-07 14:57:49 +0100

    Help knap virker!

--- main.js
+++ main.js
@@ -9,3 +9,11 @@
    //var col=document.getElementById("breadcrumbing1");
    //col.style.backgroundColor="#00000000";
    //}
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
