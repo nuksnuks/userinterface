810827b5f53c1fbb309269ac6d8018a860b774a0
soklope <skp2104@hotmail.com>
2022-03-07 13:51:31 +0100

    fix

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
 console.log("tough times never lasts...");
-
-function display() {
-   
-    var col = document.getElementsByClassName("breadcrumbing1");
-    col.style.backgroundColor= "white";
-    }
