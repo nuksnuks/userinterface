2e4c752a2f8431f227e548f8663058bdbad3b651
soklope <skp2104@hotmail.com>
2022-03-07 14:22:57 +0100

    js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("tough times never lasts...");
+//function forward() {
+
+//var col=document.getElementById("breadcrumbing1");
+//col.style.backgroundColor="#00454E";
+//}
+
+//function backward() {
+
+   //var col=document.getElementById("breadcrumbing1");
+   //col.style.backgroundColor="#00000000";
+   //}
