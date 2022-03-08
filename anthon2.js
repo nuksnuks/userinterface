6d40c6448c26b4c2d2876675478860b923d976a4
soklope <skp2104@hotmail.com>
2022-03-08 10:38:12 +0100

    nye js filer

---
+++ anthon2.js
@@ -0,0 +1,47 @@
+// Guide 4
+const breadcrumbs = [breadcrumbOne, breadcrumbTwo, breadcrumbThree]
+
+var i = 0;
+const images = [
+  'billeder/placeholders/placeholder1.png',
+  'billeder/placeholders/placeholder2.png',
+  'billeder/placeholders/placeholder3.png'
+];
+
+  function forward(){
+
+    if(i < images.length - 1){
+      i++;
+    }
+
+    document.slide.src = images[i];
+    breadcrumbs[i].style.backgroundColor = "#00454E";
+  }
+  window.onload = forward;
+
+  function backward(){
+      i--;
+    if(i < 0){
+      i = 0
+    }
+    document.slide.src = images[i];
+    breadcrumbs[i+1].style.backgroundColor = null;
+  }
+  window.onload = backward;
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+  //alert("dette er en alert, er du glad Thomas?")
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+breadcrumbOne.style.backgroundColor = "#00454E";
+
+// const backward = document.getElementById("left");
+// const forward = document.getElementById("right");
+// const breadcrumbOne = document.getElementsByClassName("breadcrumbing1");
+// const breadcrumbTwo = document.getElementById("breadcrumbTwo");
+// const breadcrumbThree = document.getElementById("breadcrumbThree");
