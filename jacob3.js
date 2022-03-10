97cb681a021018ced0f4f906391b7b0abd623147
soklope <skp2104@hotmail.com>
2022-03-10 10:36:21 +0100

    Bottom nav update

--- jacob3.js
+++ jacob3.js
@@ -61,6 +61,23 @@ function off() {
 
 breadcrumbOne.style.backgroundColor = "#00454E";
 
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 // const backward = document.getElementById("left");
 // const forward = document.getElementById("right");
 // const breadcrumbOne = document.getElementsByClassName("breadcrumbing1");
