9531969919f5698e0ff187afdb820033f373da18
soklope <skp2104@hotmail.com>
2022-03-10 10:29:55 +0100

    Update anna1.js

--- anna1.js
+++ anna1.js
@@ -84,6 +84,24 @@ function off() {
 
 breadcrumbOne.style.backgroundColor = "#00454E";
 
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 // const backward = document.getElementById("left");
 // const forward = document.getElementById("right");
 // const breadcrumbOne = document.getElementsByClassName("breadcrumbing1");
