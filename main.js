640ba10421aaa98724a9dfe31091e547d85860d1
soklope <skp2104@hotmail.com>
2022-03-10 10:29:15 +0100

    Revert "Update main.js"
    This reverts commit b9b25a57dde19a18ffee514fbd7cc766c8782e22.

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+// Guide 4
+  const breadcrumbs = [breadcrumbOne, breadcrumbTwo, breadcrumbThree]
+
+  var i = 0;
+  const images = [
+    'billeder/placeholders/placeholder1.png',
+    'billeder/placeholders/placeholder2.png',
+    'billeder/placeholders/placeholder3.png'
+  ];
+
+    function forward(){
+
+      if(i < images.length - 1){
+        i++;
+      }
+
+      document.slide.src = images[i];
+      breadcrumbs[i].style.backgroundColor = "#00454E";
+    }
+    window.onload = forward;
+
+    function backward(){
+        i--;
+      if(i < 0){
+        i = 0
+      }
+      document.slide.src = images[i];
+      breadcrumbs[i+1].style.backgroundColor = null;
+    }
+    window.onload = backward;
+
   function on() {
     document.getElementById("overlay").style.display = "block";
     //alert("dette er en alert, er du glad Thomas?")
@@ -6,6 +37,8 @@
   function off() {
     document.getElementById("overlay").style.display = "none";
   }
+
+  breadcrumbOne.style.backgroundColor = "#00454E";
 
   function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
@@ -27,6 +60,6 @@ window.onclick = function(event) {
 
 // const backward = document.getElementById("left");
 // const forward = document.getElementById("right");
-// const breadcrumbOne = document.getElementsByClassName("breadcrumbOne");
+// const breadcrumbOne = document.getElementsByClassName("breadcrumbing1");
 // const breadcrumbTwo = document.getElementById("breadcrumbTwo");
 // const breadcrumbThree = document.getElementById("breadcrumbThree");
